c7bf3fcb5d440d10d5c17a54317a05169653c40b
Mateusz Karwat <mateuszkarwat@gmail.com>
2018-06-30 19:09:11 +0200

    Add `UICollectionView+Register` extension

---
+++ Extensions/UICollectionView+Register/Protocols/ReusableView.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+protocol ReusableView: AnyObject {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableView { }
+extension UICollectionViewCell: ReusableView { }
